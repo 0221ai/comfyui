aa617aea87a514b7c0b58490c8d34d06c741b8bf
0221ai <hanjs0221ai@gmail.com>
2026-01-30 15:55:16 +0900

    Update default.sh

--- config/provisioning/default.sh
+++ config/provisioning/default.sh
@@ -34,7 +34,7 @@ UNET_MODELS=(
 
 LORA_MODELS=(
     #"https://civitai.com/api/download/models/16576"
-    "https://civitai.com/api/download/models/1192123?type=Model&format=SafeTensor&token=57ad1d485bf80287a6c4e9680cccfd6e" # Excessive XXX (or XXXX), XXXXXX - AnXL,Pony,Noob: noob1.1 high influence
+    "https://civitai.com/api/download/models/1192123?type=Model&format=SafeTensor" # Excessive XXX (or XXXX), XXXXXX - AnXL,Pony,Noob: noob1.1 high influence
 )
 
 VAE_MODELS=(
