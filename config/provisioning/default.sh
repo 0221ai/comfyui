4514b2b1bdf7b403824df5d1c8f334f9ea009c59
0221ai <hanjs0221ai@gmail.com>
2026-01-30 16:53:51 +0900

    Update default.sh

--- config/provisioning/default.sh
+++ config/provisioning/default.sh
@@ -35,7 +35,7 @@ UNET_MODELS=(
 
 LORA_MODELS=(
     #"https://civitai.com/api/download/models/16576"
-    "https://civitai.com/api/download/models/1192123" # Excessive XXX (or XXXX), XXXXXX - AnXL,Pony,Noob: noob1.1 high influence
+    "https://civitai.com/api/download/models/1192123?token=531461348ebfb873e4954b6d165590e0" # Excessive XXX (or XXXX), XXXXXX - AnXL,Pony,Noob: noob1.1 high influence
     #"https://huggingface.co/Fluffypanzer/UterusShape/resolve/main/Shaped_Uterus.safetensors" # Fluffypanzer/UterusShape
 )
 
